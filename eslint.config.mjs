5c90a76021c9772dd975c732f1a901874b178631
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-01 07:12:56 -0700

    create repo and setup project skeleton

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+export default [
+  {
+    ignores: [
+      'eslint.config.mjs',
+      'webpack.config.js',
+    ]
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+
+    },
+    rules: {
+      "@stylistic/js/space-before-function-paren": ["off", 'never'],
+      '@stylistic/js/space-before-function-paren': ["off", {
+        "named": "never"
+      }],
+      '@stylistic/js/quotes': ['warn', 'single', { avoidEscape: true }],
+      '@stylistic/js/eol-last': ['off', 'never'],
+      '@stylistic/js/function-paren-newline': ["warn", { "minItems": 3 }],
+      '@stylistic/js/object-curly-spacing': ['warn', 'always'],
+      '@stylistic/js/multiline-comment-style': ['off', 'separate-lines'],
+      '@stylistic/js/function-call-argument-newline': ["error", "consistent"],
+      '@stylistic/js/indent': ['warn', 4, { "SwitchCase": 1 }],
+      '@stylistic/js/array-element-newline': ["error", { "minItems": 3 }]
+
+    },
+    languageOptions: { globals: globals.browser }
+  },
+  stylisticJs.configs["all-flat"],
+  // pluginJs.configs.recommended,
+];
